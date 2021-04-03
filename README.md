5f66f28a10e8ae15527680202aa2933842889c7d
HankWyatt <lwyatt.henry@gmail.com>
2021-04-02 22:59:11 -0400

    Initial Discord Bot

---
+++ README.md
@@ -0,0 +1,19 @@
+## Requirements
+
+- [Node.js](http://nodejs.org/)
+- [Discord](https://discordapp.com/) account
+
+## Installation Steps (if applicable)
+
+1. Clone the SafeMoon Discord-Bot repository.
+2. Run `npm install`
+3. Run `npm i -g nodemon` (Optional)
+4. Add your discord credentials in a `.env` file. (DISCORD_TOKEN=YOUR_TOKEN_GOES_HERE)
+5. Run `npm run start`
+6. Woo. Your bot is now running.
+
+## How to add the bot to your discord server
+
+1. Create your bot and go to the oauth page and grab your bot client id.
+2. Go to: https://discord.com/oauth2/authorize?client_id=YOUR_BOT_CLIENT_ID_HERE&scope=bot
+3. Authorize it and you're good to go.
---
+++ src/bot.js
@@ -0,0 +1,173 @@
+//Dependencies.
+const { Client } = require('discord.js');
+const dotenv = require('dotenv');
+const axios = require('axios').default;
+const contractAddress = "0x8076c74c5e3f5852037f31ff0093eeb8c8add8d3"; //SAFEMOON Contract Address
+
+//Load Environment
+dotenv.config();
+console.log(process.env.DISCORD_TOKEN);
+
+//Create an instance of client
+const client = new Client();
+
+//Login
+client.login(process.env.DISCORD_TOKEN);
+client.on('ready', () => {
+    console.log(client.user.tag + ' has logged in.');
+});
+
+/**
+ * Function for obtaining data from Dex.Guru's API.
+ * @returns Dex.Guru's API data
+ */
+async function getApi() {
+    try {
+        let response = await axios.get('https://api.dex.guru/v1/tokens/' + contractAddress + '-bsc/');
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return "Failed";
+    }
+}
+
+/**
+ * Function for obtaining the total burned supply from BSCSCAN.
+ * @returns total Burned Supply to-date.
+ */
+async function getBurnedTotal() {
+    try {
+        let response = await axios.get('https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x8076C74C5e3F5852037F31Ff0093Eeb8c8ADd8D3&address=0x0000000000000000000000000000000000000001&tag=latest&apikey=YOUR_API_KEY_GOES_HERE');
+        let value = response.data['result'];
+        value = (value / 1_000_000_000_000_000_000_000).toFixed(4);
+        console.log(value);
+        return value;
+    } catch (err) {
+        console.log(err);
+        return "Failed";
+    }
+}
+
+/**
+ * Function for obtaining data from CoinMarketCap's Api.
+ * @returns CoinMarketCap's widget API json data
+ */
+async function getCMCData() {
+    try {
+        let response = await axios.get('https://3rdparty-apis.coinmarketcap.com/v1/cryptocurrency/widget?id=8757');
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return "Failed"
+    }
+}
+
+/**
+ * Function for sending the stand-alone price every 20 seconds.
+ */
+setInterval(async function () {
+    try {
+        let dexGuruData = await getApi();
+        let price = dexGuruData['priceUSD'];
+        price *= Math.pow(10, 10);
+        let channel = client.channels.cache.get('824212242480103445'); //Change this to your Channel Id
+        channel.send(price.toPrecision(6));
+    } catch (err) {
+        console.log(err);
+        return "Failed";
+    }
+}, 20 * 1000);
+
+/**
+ * Function for sending the Embedded price display every 5 minutes.
+ */
+setInterval(async function () {
+    try {
+        let dexGuruData = await getApi();
+        let price = dexGuruData['priceUSD'].toFixed(dexGuruData['decimals']);
+        let volume = (dexGuruData['volume24hUSD'] / 1_000_000).toFixed(4);
+        let channel = client.channels.cache.get('824212242480103445');
+
+        let burnTotal = await getBurnedTotal();
+        let timeStamp = Date.now();
+
+        let cmcData = await getCMCData();
+        let cmcBase = cmcData.data[8757];
+        let cmcQuote = cmcBase['quote']['USD'];
+        let total_supply = cmcBase['total_supply'];
+        let marketCap = ((total_supply * price) / 1_000_000).toFixed(4);
+
+        let change1h = cmcQuote['percent_change_1h'].toFixed(4);
+        let change24h = cmcQuote['percent_change_24h'].toFixed(4);
+        let change7d = cmcQuote['percent_change_7d'].toFixed(4);
+
+        channel.send({
+            embed: {
+                "title": "**" + contractAddress + "**",
+                "description": "This bot will automatically post new stats every 5 minutes.",
+                "url": "https://bscscan.com/address/" + contractAddress,
+                "color": 2029249,
+                "timestamp": timeStamp,
+                "footer": {
+                    "text": "SafeMoon Price Bot - Values based on USD."
+                },
+                "thumbnail": {
+                    "url": "https://i.imgur.com/cAjC1Pz.png"
+                },
+                "author": {
+                    "name": "SafeMoon Price Bot",
+                    "url": "https://safemoon.net"
+                },
+                "fields": [
+                    {
+                        "name": "💸 Price",
+                        "value": "$" + price,
+                        "inline": true
+                    },
+                    {
+                        "name": "🧊 Volume",
+                        "value": "$" + volume + "M",
+                        "inline": true
+                    },
+                    {
+                        "name": "💰 Market Cap",
+                        "value": marketCap + "M",
+                        "inline": true
+                    },
+                    {
+                        "name": "🏦 Total Supply",
+                        "value": "1000T",
+                        "inline": true
+                    },
+                    {
+                        "name": "🔥 Total Burned",
+                        "value": burnTotal + "T",
+                        "inline": true
+                    },
+                    {
+                        "name": "💱 Circ Supply",
+                        "value": (total_supply / 1_000_000_000_000).toFixed(2) + "T",
+                        "inline": true
+                    },
+                    {
+                        "name": "💯 1hr Change",
+                        "value": change1h > 0 ? "⬆️ " + change1h + "%" : "⬇️ " + change1h + "%",
+                        "inline": true
+                    },
+                    {
+                        "name": "📈 24hr Change",
+                        "value": change24h > 0 ? "⬆️ " + change24h + "%" : "⬇️ " + change24h + "%",
+                        "inline": true
+                    },
+                    {
+                        "name": "📈 7D Change",
+                        "value": change7d > 0 ? "⬆️ " + change7d + "%" : "⬇️ " + change7d + "%",
+                        "inline": true
+                    }
+                ]
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}, 300 * 1000); //(x * 1000) this will post in the designated channel every 'x' seconds.
