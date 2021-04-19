237916da6bd97d94af94f97b75a7e53523a72574
Reece Benson <business@reecebenson.me>
2021-04-19 12:37:15 +0000

    chore: containerise bot for development

--- src/mongo.js
+++ src/mongo.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const { mongoPath } = require('./config.json')
+const { MONGO_INITDB_DATABASE_NAME } = process.env
 
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
         useFindAndModify: false,
         useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    return mongoose;
+        useUnifiedTopology: true,
+        dbName: MONGO_INITDB_DATABASE_NAME || 'discordbot'
+    })
+    return mongoose
 }
